0d1116ac340552fe78714a77975b473c229acbda
Ian Homer <ian@purplepip.com>
2020-06-28 12:11:44 +0100

    LCD

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+package=${1:-"lcd"}
+echo "Deploying $package"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR
+cd $DIR/$1
 
 arduino-cli compile --fqbn arduino:avr:mega
 arduino-cli upload -p /dev/cu.usbmodem14301 --fqbn arduino:avr:mega
